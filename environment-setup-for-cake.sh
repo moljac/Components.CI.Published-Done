95dd745818e25dc9d865369bf6dff67e1fc8dca2
moljac <mcvjetko@holisticware.net>
2016-03-10 10:06:20 +0100

    cake shell script

---
+++ environment-setup-for-cake.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export PATH=/Library/Frameworks/Mono.framework/Commands:$PATH
+
+alias xbuild="/Library/Frameworks/Mono.framework/Commands/xbuild"
+alias XBuild="/Library/Frameworks/Mono.framework/Commands/xbuild"
